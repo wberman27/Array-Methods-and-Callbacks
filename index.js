c2e76ddf9692ac13ff36bead5b2a8f6f0c69d591
Will B <wberman27@gmail.com>
2021-01-26 14:16:43 -0500

    added completed task 1

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ Practice accessing data by console.log-ing the following pieces of data note, yo
 const finals2014 = fifaData.filter((item) => item.Year === 2014 && item.Stage === "Final");
 console.log(finals2014);
 //(a) Home Team name for 2014 world cup final
-console.log(finals2014["Home Team Name"]);
+console.log(finals2014[0]["Home Team Name"]);
 //(b) Away Team name for 2014 world cup final
-console.log(finals2014["Away Team Name"]);
+console.log(finals2014[0]["Away Team Name"]);
 //(c) Home Team goals for 2014 world cup final
-
+console.log(finals2014[0]["Home Team Goals"]);
 //(d) Away Team goals for 2014 world cup final
-
+console.log(finals2014[0]["Away Team Goals"]);
 //(e) Winner of 2014 world cup final */
-
+console.log(finals2014[0]["Win conditions"].split("win")[0]);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
